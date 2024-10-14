9f1b8ff8cfda335f00f51d52ca88b6589581f241
Doluwamu <dboyadeitan@gmail.com>
2024-10-14 14:23:41 +0100

    made update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <section className="flex flex-col items-center gap-8 py-4">
       <p className="text-center">
-        &copy; <span>2023, Doluwamu.</span>
+        &copy; <span>2024, Doluwamu.</span>
       </p>
     </section>
   );
